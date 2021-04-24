5da9f025b8829ab616366c3a05b272fadd1717af
jacoposcuola <jacopo.scuola0@gmail.com>
2021-04-24 14:23:53 +0200

    Add Some Things (player)

---
+++ Assets/Scripts/Editor/PlayerBoundScreenLimit.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ScreenLimit))]
+public class PlayerBoundScreenLimit : Editor
+{
+    /// <summary>
+    /// usati per tener conto della dimensione totale man mano si posizionano i background,
+    /// in modo da non andare a misurare ogni volta con i gameObject
+    /// </summary>
+    /// 
+    Vector2 dira;
+   
+    public override void OnInspectorGUI()
+    {
+        ScreenLimit screenLimit = (ScreenLimit)target;
+        
+        DrawDefaultInspector();
+
+        //Handles.DrawLine(screenLimit.upDx, screenLimit.downDx);
+    }
+
+
+    public void OnSceneGUI()
+    {
+        ScreenLimit screenLimit = (ScreenLimit)target;
+
+        Handles.DrawLine(screenLimit.upDx, screenLimit.downDx);
+        Handles.DrawLine(screenLimit.downDx, screenLimit.downSx);
+        Handles.DrawLine(screenLimit.downSx, screenLimit.upSx);
+        Handles.DrawLine(screenLimit.upSx, screenLimit.upDx);
+        
+    }
+
+
+
+}
---
+++ Assets/Scripts/Game/ScreenLimit/ScreenLimit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ScreenLimit : MonoBehaviour
+{
+   // collider che andranno applicati come limite a fine lavoro(test)
+    
+    public BoxCollider2D dx;
+    public BoxCollider2D sx;
+    public BoxCollider2D up;
+    public BoxCollider2D down;
+
+    // vettori di dimensione schermo
+   public Vector2 upDx;
+   public Vector2 downDx;
+   public Vector2 downSx;
+   public Vector2 upSx;
+
+
+    void All()
+    {
+        //down.size = spacing;
+
+     
+
+    }
+
+    private void Update()
+    {
+        All();
+        
+    }
+
+
+}
